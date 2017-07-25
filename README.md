ca662d0c971ab78c5732d9215c86a9eee757d0bd
hy <huyuan2016@yeah.net>
2017-07-25 17:23:00 +0800

    增加等号模式

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
-# formula.js
+创建组件：        $('#container').formula()
 
-> 一个jQueryUI的可拖拽生成表达式小插件
+调用组件外部方法： $('#container').formula('方法名')
+
+```
+参数：{
+
+		arg：[{
+				name:'字段1',
+				id:'f1'
+			},{
+				name:'字段2',
+				id:'f2'
+			}...],//字段
+
+		symbol:[{
+				name:'加+',
+				sign:'+'
+			},{
+
+			}...],//运算符
+
+		addField: { //增加常量模块的开关
+			addable: true,//默认为开
+			text: 'button文本'
+		},
+
+		disp: '' //表达式缓存，设置后可以初始化表达式内容
+	}
+
+外部api:
+	getFormulaArr()：     //获得公式数组
+	getFormulaStr([arr])：//获得公式字符串 arr参数可以不给，自动调用getFormulaArr
+	checkout():           //检查公式 ,返回
+								{
+									result: false/true,
+									msg: ''      //错误信息
+								}
+内部api:
+	_bindEvent():    //组件init时绑定的拖拽点击事件
+	_inSymbol(str):  //判断参数是否在运算符内  返回true/false
+	_parse([str]):        //编译参数disp为数组 str参数未给出时，会自动调用getFormulaStr
+	_parseArg(str)：      //编译str,返回用参数arg下标('a'+i)替换的字符串
+	_parseSymbol(str)：   //编译str,返回用参数symbol下标('s'+i)替换的字符串
+	_toHTML(arr)：        //把字段和运算符下标数组转换为html  返回字符串类型的html
+```
--- js/formula.js
+++ js/formula.js
@@ -1,4 +1,4 @@
-$.widget("custom.formula", {
+$.widget("bangboss.formula", {
      //默认参数
     options: {
         arg: [{
@@ -19,17 +19,19 @@ $.widget("custom.formula", {
                 sign: '-'
             }, {
                 name: '乘×',
-                sign: '*'
+                sign: '×'
             }, {
                 name: '除÷',
-                sign: '/'
+                sign: '÷'
             }, {
                 name: '汇总',
                 sign: '[sum,'
-            }, {
-                name: '计数',
-                sign: '[count,'
-            }, {
+            }
+            // , {
+            //     name: '计数',
+            //     sign: '[count,'
+            // }
+            , {
                 name: '左括号(',
                 sign: '('
             }, {
@@ -38,14 +40,16 @@ $.widget("custom.formula", {
             }],
         addField: {
             addable: true,
-            text: '增加常量'
+            text: '增加常量'  //button上的文字
         },
-        disp: '' //表达式缓存
+        disp: '', //表达式缓存
+        mode: 'default'  // default,equation
     },
     _create: function () { //初始化，控件生命周期内只运行一次
         console.log('组件create')
 
-        var html ='<div class="expr-elements"><div>字段: </div> <ul class="expr-fields"></ul><div class="add-field"></div></div><div class="expr-elements"><div>运算符</div><ul class="expr-symbol"></ul></div><div><div class = "expr-leftop">公式为：<span class="preViewRes"></span></div><ul class="expr-disp"></ul><div class = "expr-error"></div><button class="test">test</button></div>'
+        var html = '<div class="expr-elements"><div>字段: </div> <ul class="expr-fields"></ul><div class="add-field"></div></div><div class="expr-elements"><div>运算符</div><ul class="expr-symbol"></ul></div><div><div class = "expr-leftop">公式为：<span class="preViewRes"></span></div>'+(this.options.mode=='default'?'':'<ul class="expr-equ"></ul>=')+'<ul class="expr-disp"></ul><div class = "expr-error"></div><button class="test">test</button></div>'
+
         this.element.html(html)
     },
     //创建控件，控件生命周期内会运行多次
@@ -83,7 +87,7 @@ $.widget("custom.formula", {
             var str = me.getFormulaStr()
             $('.preViewRes', $el).html(str)
             var res = me.checkout()
-            if (res.result) {
+            if (res.success) {
                 $('.expr-error', $el).html('')
             }
             else {
@@ -96,55 +100,86 @@ $.widget("custom.formula", {
             console.log(op.disp)
             var h = this._parse(op.disp)
             console.log(h)
-            $('.expr-disp', $el).html(this._toHTML(h))
+            if (op.mode == 'equation') {
+                var o = this._toHTML(h)
+                $('.expr-equ', $el).html(o.equ)
+                $('.expr-disp', $el).html(o.disp)
+            } else {
+                $('.expr-disp', $el).html(this._toHTML(h))
+            }
         }
     },
     getFormulaArr:function(){//获取公式数组 Array
         var res = []
+        //equation模式
+        if (this.options.mode == 'equation') {
+            res.push($('.expr-equ li', this.element).attr('data-value'))
+            res.push('=')
+        }
+
         $('.expr-disp li', this.element).each(function (i, v) {
             res.push(v.getAttribute('data-value'))
         })
         return res
     },
-    getFormulaStr: function (arr) {
-        arr = arr || this.getFormulaArr()
+    getFormulaStr: function (bool) {//bool为true时 称号和除号都转化为 * / 模式
+        arr = this.getFormulaArr()
         $.each(arr, function (i, v) {
             if (v == '[sum,') {
-                arr.splice(i+2,0,']')
-            } else if (v == '[count,') {
                 arr.splice(i + 2, 0, ']')
+            }
+            // } else if (v == '[count,') {
+            //     arr.splice(i + 2, 0, ']')
+            // }
+            if (bool) {
+                if (v == '×') {
+                    arr[i]='*'
+                } else if (v =='÷'){
+                    arr[i]='/'
+                }
             }
         })
         return arr.join('')
     },
     checkout: function () { //检查公式是否合法 返回布尔型
         var arr = this.getFormulaArr()
+        if (this.options.mode == 'equation') {
+            if (arr[0] == undefined || arr[0]=='=') {
+                return {
+                    success: false,
+                    msg: '等式左边必须要有个字段'
+                }
+            }
+            arr.splice(0,2)
+        }
         if (arr.length == 0) return {
             result: true,
             msg: ''
         }
         for (var i = 0; i < arr.length; i++){
             if (this._inSymbol(arr[i])) {//运算符
-                if (arr[i] == '[count,' || arr[i] == '[sum,') {
+                // if (arr[i] == '[count,' || arr[i] == '[sum,') {
+                if ( arr[i] == '[sum,') {
                     if (i == arr.length-1) return {
-                        result: false,
+                        success: false,
                         msg: '汇总或者计数后面必须需要一个字段'
                     }
                     if (this._inSymbol(arr[i + 1])) {
-                        return { result: false, msg:'汇总或者计数后面只能跟字段'}
+                        return { success: false, msg:'汇总或者计数后面只能跟字段'}
                     } else {
                         arr.splice(i, 2, '(',1,')')
                         // if(!this._inSymbol(arr[i+1]))
                     }
-                } else if (arr[i] == '+' || arr[i] == '-' || arr[i] == '*' || arr[i] == '/') {
+                } else if (arr[i] == '+' || arr[i] == '-' || arr[i] == '×' || arr[i] == '÷') {
                     arr[i]='+'
                 }
             } else {//字段
                 if (i + 1 < arr.length) {
                     if (this._inSymbol(arr[i + 1])) {
-                        if (arr[i + 1]=='[count,'||arr[i+1]=='[sum,') {
+                        // if (arr[i + 1]=='[count,'||arr[i+1]=='[sum,') {
+                        if (arr[i+1]=='[sum,') {
                             return {
-                                result: false,
+                                success: false,
                                 msg: '字段后面不能跟汇总或计数'
                             }
                         }
@@ -152,7 +187,7 @@ $.widget("custom.formula", {
 
                     } else {
                         return {
-                            result: false,
+                            success: false,
                             msg: '两个字段不能相邻'
                         }
                     }
@@ -164,15 +199,48 @@ $.widget("custom.formula", {
             var result = eval(arr.join(''))
         } catch(err){
             return {
-                result: false,
+                success: false,
                 msg: '计算公式不符合运算规则'
             }
         }
         if(typeof result=='number'&&!isNaN(result))
             return {
-                result: true,
+                success: true,
                 msg: ''
             }
+    },
+    calc:function(str,o){//str为表达式 o为传入的数据{id1:number,id2:number}
+        var res
+        for (var k in o) {
+            var reg = new RegExp(k, 'g')
+            if (reg.test(str)) {
+                str=str.replace(reg,o[k])
+            }
+        }
+        str = this._parseSum(str)
+        console.log(str)
+        try {
+             res=eval(str)
+        } catch (err) {
+            throw new Error('亲，您的表达式错误啦')
+            return
+        }
+        return res
+    },
+    sum: function () {
+        var arg = arguments,sum=0
+        for (var i = 0; i < arg.length; i++){
+            sum+=arg[i]
+        }
+        return sum
+    },
+    _parseSum: function (str) {
+        var reg =/\[sum,/g
+        if (reg.test(str)) {
+            str=str.replace(reg,'this.sum(')
+        }
+        str = str.replace(/]/g, ')')
+        return str
     },
     _bindEvent: function () {//绑定拖拽点击事件
         var me = this,
@@ -181,16 +249,53 @@ $.widget("custom.formula", {
             revert: "invalid",
             helper: 'clone',
             containment: this.element,
-            connectToSortable: '.expr-disp'
+            connectToSortable: '.expr-disp,.expr-equ'
         }).disableSelection().click(function (e) {
-            $('.expr-disp', $el).append($(this).clone().append('<span class="expr-colse" onclick="$(this).parent().remove()">&times;</span>'))
+            var el, $me = $(this)
+            if ($me.hasClass('expr-element')) {//符号
+                if ($me.text() == '汇总') {
+                    el = $me.clone(false).append('<span class="expr-colse" onclick="$(this).parent().remove()">&times;</span>')
+                } else {
+                    el = $me.clone(false).html($me.attr('data-value') + '<span class="expr-colse" onclick="$(this).parent().remove()">&times;</span>')
+                }
+                    $('.expr-disp', $el).append(el)
+            }else{//字段
+                el = $me.clone(false).append('<span class="expr-colse" onclick="$(this).parent().remove()">&times;</span>')
+                if ($('.expr-equ li', $el).length == 0) {
+                    $('.expr-equ', $el).append(el)
+                } else {
+                    $('.expr-disp', $el).append(el)
+                }
+            }
+
         })
         $('.expr-disp', $el).empty().sortable({
             cancel: ".expr-colse",
+            // connectWith: '.expr-equ',
             receive: function (e, ui) {
+                var li = ui.helper,text = li.text()
+
+                if (me._inSymbol(text, true) && text != '汇总') {
+                    li.text(li.attr('data-value')).attr('style', '')
+                }
                 ui.helper.append('<span class="expr-colse" onclick="$(this).parent().remove()">&times;</span>')
             }
         })
+
+        if (this.options.mode == 'equation') { //等号模式
+            $('.expr-element', $el).draggable("option", "connectToSortable", '.expr-disp');
+            $('.expr-equ', $el).empty().sortable({
+                cancel: ".expr-colse",
+                // connectWith:'.expr-disp',
+                receive: function (e, ui) {
+                    if ($('li', this).length>0) {
+                        $(this).html(ui.helper.append('<span class="expr-colse" onclick="$(this).parent().remove()">&times;</span>'))
+                    } else {
+                        ui.helper.append('<span class="expr-colse" onclick="$(this).parent().remove()">&times;</span>')
+                    }
+                }
+            })
+        }
 
         //add按钮
         if(this.options.addField.addable)
@@ -214,41 +319,78 @@ $.widget("custom.formula", {
                 }
             })
     },
-    _inSymbol: function (sign) {
-        var symbol=this.options.symbol
+    _inSymbol: function (str,bool) {
+        var symbol = this.options.symbol,
+            k=bool?'name':'sign'
         for (var i = 0; i < symbol.length; i++){
-            var sym = symbol[i]
-            if (sym.sign == sign) {
-                return true
-            }
+                var sym = symbol[i]
+                if (sym[k] == str) {
+                    return true
+                }
+
         }
         return false
     },
     _parse: function (str) {//解析表达式 如 1+2+[sum,e] 解析为参数下标
         var symbol = this.options.symbol
 
+        str=str.replace(/[\*]/g,'×')
+        str=str.replace(/[\/]/g,'÷')
+
         str = this._parseSymbol(this._parseArg(str))
+
+        str = str.replace(/=/g, ' = ')
+
+        console.log(str)
 
         str = $.trim(str)
         var arr = str.split(/\s+/)
 
         return arr
     },
-    _toHTML: function (arr) {//下标数组转换为html
-        var html='',arg=this.options.arg,symbol=this.options.symbol
-        for(var i=0;i<arr.length;i++){
-            var v=arr[i]
-            if(v[0]=='a'){
-                var k=v[1]
-                html+='<li data-value="' +arg[k].id + '" class="expr-field">' + arg[k].name + '<span class="expr-colse" onclick="$(this).parent().remove()">×</span></li>'
-            }else if(v[0]=='s'){
-                var k=v[1]
-                html+='<li data-value="'+symbol[k].sign+'" class="expr-element">'+symbol[k].name+'<span class="expr-colse" onclick="$(this).parent().remove()">×</span></li>'
-            }else{
-                html+='<li data-value="'+v+'" class="expr-field">'+v+'<span class="expr-colse" onclick="$(this).parent().remove()">×</span></li>'
+    _toHTML: function (arr1) {//下标数组转换为html
+        var arr=this._doit(arr1)
+        if (this.options.mode == 'equation') {
+            var i = $.inArray('=', arr)
+            if (i == 0) {
+                return {
+                    equ: '',
+                    disp:arr.slice(1).join('')
+                }
+            } else if (i == 1) {
+                return {
+                    equ: arr[0],
+                    disp: arr.slice(2).join('')
+                }
+
+            } else if(i==-1){
+                return {
+                    equ: '',
+                    disp: arr.join('')
+                }
+            } else {
+                throw('在等式模式下，请给出正确的等式参数')
+            }
+
+        } else {
+            return arr.join('')
+        }
+    },
+    _doit: function (arr) {
+        var  arg = this.options.arg, symbol = this.options.symbol
+        for (var i = 0; i < arr.length; i++) {
+            var v = arr[i]
+            if (v[0] == 'a') {
+                var k = v[1]
+                arr[i]= '<li data-value="'+arg[k].id+'" class="expr-field">'+arg[k].name+'<span class="expr-colse" onclick="$(this).parent().remove()">×</span></li>'
+            } else if (v[0] == 's') {
+                var k = v[1]
+                arr[i]= '<li data-value="'+symbol[k].sign+'" class="expr-element">'+(symbol[k].sign=='[sum,'?'汇总':symbol[k].sign)+'<span class="expr-colse" onclick="$(this).parent().remove()">×</span></li>'
+            } else if(v!='='){
+                arr[i]= '<li data-value="'+v+'" class="expr-field">'+v+'<span class="expr-colse" onclick="$(this).parent().remove()">×</span></li>'
             }
         }
-        return html
+        return arr
     },
     _parseArg: function (str) {
         var arg = this.options.arg
